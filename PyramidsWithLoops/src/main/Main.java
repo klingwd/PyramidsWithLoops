9d0368b1f752728a9e07a8a1a3e72fe2584d3408
williamkling <williamkling@williams-mbp-7.home>
2019-09-25 22:06:06 -0400

    Correction to for loop

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,6 +1,6 @@
 /* 
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
+ * William Kling
+ * klingwd@mail.uc.edu
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
         1
@@ -14,17 +14,20 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 1; i <= lines; i++) {
-			
-			for (int j = 0; j <= i+lines; j++) {
-				System.out.print("");
-				System.out.print(j);
-			for (int k = i; k <= i; k--) {
-				System.out.print(k);
-			}
-			System.out.println("");
-		}
+		int lines = 5;  // Number of Lines you want in Pyramid 
+		int k = 0;
+		
+	    for(int i = 1; i <= lines; i++, k = 0) {   // "i" needs to be <= the number of lines and not < or else you will only have 4 lines 
+	        	
+	            for(int j= 0; j < lines - i; j++) { // lines - i to format correctly 
+	                System.out.print(" "); 
+	            }
+	            while(k != 2 * i - 1) {     // k not equal to 2, multiples by 1 
+	                System.out.print(i);
+	                k++;  // adds 1 to the variable k
+	            }
+	            System.out.println();
+	        }
+	    }
 	}
-	}}
 
